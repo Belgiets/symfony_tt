929a6f74939d0ed59db0a4a44f1b0f5aa102f331
belgiets <chegidim@gmail.com>
2016-02-29 22:00:56 +0200

    added form to create a new user

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -2,8 +2,13 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Person;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends Controller
 {
@@ -50,6 +55,38 @@ class AdminController extends Controller
         return $this->render('admin/status.html.twig', [
             'person'    => $person,
             'operation' => 'delete'
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="user_create")
+     */
+    public function createAction(Request $request)
+    {
+        $person = new Person();
+
+        $form = $this->createFormBuilder($person)
+            ->add('fname', TextType::class)
+            ->add('lname', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('save', SubmitType::class, ['label' => 'Create user'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($person);
+            $em->flush();
+
+            return $this->render('admin/status.html.twig', [
+                'person'    => $person,
+                'operation' => 'create'
+            ]);
+        }
+
+        return $this->render('admin/create.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
