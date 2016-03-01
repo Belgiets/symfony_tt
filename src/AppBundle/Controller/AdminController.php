62f8bac940a938a476a84a2e1de698f9e5168706
belgiets <chegidim@gmail.com>
2016-03-01 21:01:21 +0200

    renamed views accorinally to actions, added create user action, added sending mail and saving messages, fixed deleting related data, fixed adding multiple messages

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -2,11 +2,13 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Message;
 use AppBundle\Entity\Person;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -21,7 +23,7 @@ class AdminController extends Controller
         $persons = $em->getRepository('AppBundle:Person')
             ->findAllOrderedByName();
 
-        return $this->render('admin/show.html.twig', [
+        return $this->render('admin/admin.html.twig', [
             'persons' => $persons
         ]);
     }
@@ -29,14 +31,49 @@ class AdminController extends Controller
     /**
      * @Route("/show/{userId}", name="user_show")
      */
-    public function showAction($userId)
+    public function showAction(Request $request, $userId)
     {
         $em = $this->getDoctrine()->getManager();
         $person = $em->getRepository('AppBundle:Person')
             ->find($userId);
 
-        return $this->render('admin/detail.html.twig', [
-            'person' => $person
+        $message = new Message();
+
+        $form = $this->createFormBuilder($message)
+            ->add('topic', TextType::class)
+            ->add('body', TextareaType::class)
+            ->add('save', SubmitType::class, ['label' => 'Send'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message->setPerson($person);
+            $em->persist($message);
+            $em->flush();
+
+            $mail = \Swift_Message::newInstance()
+                ->setSubject($message->getTopic())
+                ->setFrom('symfony@testtask.com')
+                ->setTo($person->getEmail())
+                ->setBody(
+                    $this->renderView(
+                        'admin/email.html.twig',
+                        ['message' => $message]
+                    ),
+                    'text/html'
+                );
+            $this->get('mailer')->send($mail);
+
+            return $this->render('admin/status.html.twig', [
+                'person'    => $person,
+                'operation' => 'send message'
+            ]);
+        }
+
+        return $this->render('admin/show.html.twig', [
+            'person' => $person,
+            'form'   => $form->createView()
         ]);
     }
 
